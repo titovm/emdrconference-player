8ff8ec2c01752980e54df12049bba6eeb7a8a565
Mark Titov <titov.mark@gmail.com>
2025-01-02 01:09:41 +0100

    Edge runtime export add

--- pages/api/files.js
+++ pages/api/files.js
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk';
 
+export const runtime = 'edge';
+
 export default async function handler(req, res) {
   // Validate HTTP method
   if (req.method !== 'GET') {
--- pages/api/getSignedUrl.js
+++ pages/api/getSignedUrl.js
@@ -1,5 +1,7 @@
 import AWS from 'aws-sdk';
 
+export const runtime = 'edge';
+
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
